d29593531040ee50c489d91440cc658edcb613ae
Harsh Raj <167784787+Harshrj53@users.noreply.github.com>
2025-08-26 11:00:26 +0530

    Codeforces_Checking_sol(1791A).py

--- Codeforces_Checking_sol(1791A).py
+++ Codeforces_Checking_sol(1791A).py
@@ -1,9 +1,8 @@
 t=int(input())
 for i in range(t):
     letter=input()
+    ans="No"
     al=["c","o","d","e","f","o","r","c","e","s"]
-    for i in range(len(al)):
-        if letter==al[i]:
-            print("Yes")
-        else:
-            print("No")
+    if letter in al:
+        ans="Yes"
+    print(ans)  
